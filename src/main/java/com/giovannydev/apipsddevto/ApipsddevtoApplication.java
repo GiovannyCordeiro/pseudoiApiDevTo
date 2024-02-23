c3d04a68cc3c45d696cbb16d3705c61340f10cba
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-02-23 11:02:00 -0300

    first: setup project

---
+++ src/main/java/com/giovannydev/apipsddevto/ApipsddevtoApplication.java
@@ -0,0 +1,13 @@
+package com.giovannydev.apipsddevto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipsddevtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipsddevtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovannydev/apipsddevto/ApipsddevtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannydev.apipsddevto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipsddevtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
